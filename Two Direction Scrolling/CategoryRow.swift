215a3267eb9e4a3dc8d0bd5e6e405e6d394271a4
Jorge Gallardo <jorge8486@gmail.com>
2015-08-16 11:50:26 -0400

    first commit

---
+++ Two Direction Scrolling/CategoryRow.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryRow.swift
+//  Two Direction Scrolling
+//
+//  Created by Jorge Gallardo on 8/16/15.
+//  Copyright © 2015 Jorge Gallardo. All rights reserved.
+//
+
+import UIKit
+
+class CategoryRow: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Two Direction Scrolling/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Two Direction Scrolling
+//
+//  Created by Jorge Gallardo on 8/16/15.
+//  Copyright © 2015 Jorge Gallardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    var categories = ["Action", "Drama", "Science Fiction", "Kids", "Horror"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return categories[section]
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return categories.count
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell") as! CategoryRow
+        return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
